93e53d3b9b8e3971d68bd5964fc81cd834a9c75d
Egor <egorviliavin@gmail.com>
2024-06-13 21:41:20 +0300

    t

---
+++ .nuxt/types/schema.d.ts
@@ -0,0 +1,61 @@
+import { NuxtModule, RuntimeConfig } from 'nuxt/schema'
+declare module 'nuxt/schema' {
+  interface NuxtConfig {
+    ["tailwindcss"]?: typeof import("@nuxtjs/tailwindcss").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["hub"]?: typeof import("@nuxthub/core").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["devtools"]?: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    modules?: (undefined | null | false | NuxtModule | string | [NuxtModule | string, Record<string, any>] | ["@nuxtjs/tailwindcss", Exclude<NuxtConfig["tailwindcss"], boolean>] | ["@nuxthub/core", Exclude<NuxtConfig["hub"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+  }
+  interface RuntimeConfig {
+   app: {
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+
+   hub: {
+      projectUrl: string,
+
+      projectSecretKey: string,
+
+      url: string,
+
+      projectKey: string,
+
+      userToken: string,
+
+      remote: any,
+
+      remoteManifest: any,
+
+      dir: string,
+
+      analytics: boolean,
+
+      blob: boolean,
+
+      cache: boolean,
+
+      database: boolean,
+
+      kv: boolean,
+
+      version: string,
+
+      env: string,
+
+      openapi: boolean,
+   },
+  }
+  interface PublicRuntimeConfig {
+
+  }
+}
+declare module 'vue' {
+        interface ComponentCustomProperties {
+          $config: RuntimeConfig
+        }
+      }
---
+++ dist/_worker.js/index.js
@@ -0,0 +1,2 @@
+globalThis._importMeta_={url:"file:///_entry.js",env:{}};export{O as default}from"./chunks/runtime.mjs";
+//# sourceMappingURL=index.js.map
