2c7bf24747b42a027c98e386863861917af637a0
Egor <egorviliavin@gmail.com>
2024-06-21 13:02:23 +0300

    feat: добавляет новые компоненты в приложение

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
 		<NuxtPage />
 	</NuxtLayout>
 </template>
+
+<style>
+	html {
+		scroll-behavior: smooth;
+	}
+
+	body {
+		font-family: 'Montserrat', sans-serif;
+		font-optical-sizing: auto;
+	}
+</style>
---
+++ components/about.vue
@@ -0,0 +1,94 @@
+<script setup></script>
+
+<template>
+	<div
+		class="mb-20"
+		id="about">
+		<div class="container mx-auto py-8">
+			<div
+				class="flex flex-col items-center md:items-stretch md:justify-between md:flex-row">
+				<div>
+					<img
+						src="public/images/denis.webp"
+						alt="Фотография пастора Дениса"
+						class="w-80 md:w-96 rounded-xl shadow-2xl" />
+				</div>
+				<div class="flex flex-col w-80 md:w-2/3 md:justify-between">
+					<div class="flex flex-col gap-5">
+						<h1 class="text-2xl mt-4 md:mt-0 md:text-4xl font-medium">
+							История пастора Дениса
+						</h1>
+						<h2 class="text-xl md:text-3xl">
+							Денис Орловский родился в 1980 году на Дальнем востоке России. В
+							возрасте 16 лет уехал учиться в город Благовещенск, где вскоре
+							уверовал в Господа. В столь молодом возрасте Бог так сильно
+							коснулся его сердца, что по сей день он является активным Божьим
+							служителем.
+						</h2>
+						<p class="text-l md:text-2xl text-gray-400">
+							В 2004 году вместе с женой и сыном переехал для служения
+							миссионером на север Дальнего востока, где помогал, а в
+							последствии вёл церковь в городе Тында. Служение на севере
+							продлилось около 12 лет, там же были рождены ещё двое его детей.
+						</p>
+					</div>
+					<div class="flex justify-center mt-4 md:justify-end">
+						<a
+							href="mailto:d.orlovskii@inbox.ru"
+							class="px-4 py-3 bg-blue-900 text-white rounded hover:bg-blue-700 hover:shadow-2xl transition-all cursor-pointer"
+							>Задать вопрос пастору</a
+						>
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="px-4 mt-20 flex flex-col xl:flex-row justify-between gap-7 mb-10">
+				<div>
+					<p class="max-w-96 text-l text-gray-400">
+						В 2000 году он встретил свою будущую жену и в 2001 женился. Сейчас у
+						Дениса и его супруги Анастасии трое детей.В 2004 году вместе с женой
+						и сыном переехал для служения миссионером на север Дальнего востока,
+						где помогал, а в последствии вёл церковь в городе Тында. Служение на
+						севере продлилось около 12 лет, там же были рождены ещё двое его
+						детей. В 2015 году Господь начал давать ему множество пророческих
+						переживаний, ангельских посещений и пророческих снов, в которых
+						открывал Свою волю и тайны Священного писания. Основным направлением
+						послания Дениса стало раскрытие тайн завершенной работы Господа
+						нашего на кресте.
+					</p>
+					<img
+						src="public/images/family.webp"
+						alt="Фотография семьи Орловских"
+						class="w-96 mt-4 rounded-xl shadow-2xl" />
+				</div>
+				<div class="md:ml-40">
+					<p class="text-l md:text-xl">
+						Денис с семьей служат Богу уже 25 лет. В данный момент в городе
+						Краснодар.
+					</p>
+					<img
+						src="public/images/2024-04-16-12.40.22.webp"
+						alt="Фотография Денис и Анастасия"
+						class="rounded-xl shadow-2xl mt-4" />
+					<img
+						src="public/images/family-1.webp"
+						alt="Фотография семьи Орловских"
+						class="w-80 rounded-xl shadow-2xl mt-4" />
+				</div>
+				<div class="h-100 flex flex-col md:items-end md:justify-end">
+					<p class="max-w-96 text-l text-gray-400">
+						Супруга Дениса - Анастасия, несет культуру поклонения из сердца Бога
+						Отца, поклонения, которое высвобождаясь, изменяет атмосферу вокруг.
+					</p>
+					<img
+						src="public/images/IMG-20231231-WA0009.webp"
+						alt="Фотография Денис и Анастасия"
+						class="w-96 mt-4 rounded-xl shadow-2xl" />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
