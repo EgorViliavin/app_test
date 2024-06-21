32b376cca6fc5d76bbbdad31f09d36a540f53e11
Egor <egorviliavin@gmail.com>
2024-06-21 13:56:41 +0300

    feat: новая страница 404

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@nuxthub/core'],
+	modules: ['@nuxtjs/tailwindcss', '@nuxthub/core', 'nuxt-icons'],
 
 	tailwindcss: {
 		exposeConfig: true,
